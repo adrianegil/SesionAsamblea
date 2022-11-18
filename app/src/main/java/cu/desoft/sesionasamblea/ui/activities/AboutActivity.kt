a7ea22a38e21eadb2ef077958046c97b1398c56f
Adrian E Gil <adrianegil97@gmail.com>
2022-11-18 09:30:43 -0500

    Se diseñó la pantalla de Acerca de y Descripción. Se justificó el texto del Menú Principal. Se insertó el icono de la app.

--- app/src/main/java/cu/desoft/sesionasamblea/ui/activities/AboutActivity.kt
+++ app/src/main/java/cu/desoft/sesionasamblea/ui/activities/AboutActivity.kt
@@ -1,5 +1,7 @@
 package cu.desoft.sesionasamblea.ui.activities
 
+import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import cu.desoft.sesionasamblea.databinding.ActivityAboutBinding
@@ -14,5 +16,13 @@ class AboutActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbarAboutActivity)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        binding.textViewDESOFTSiteWeb.setPaintFlags(binding.textViewDESOFTSiteWeb.getPaintFlags() or Paint.UNDERLINE_TEXT_FLAG)
+        binding.textViewDESOFTEmail.setPaintFlags(binding.textViewDESOFTEmail.getPaintFlags() or Paint.UNDERLINE_TEXT_FLAG)
+
+        binding.btnDescription.setOnClickListener {
+            startActivity(Intent(this, DescriptionActivity::class.java))
+        }
+
     }
 }
